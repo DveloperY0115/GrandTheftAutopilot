2dc8fb895969f4530e258f64704d4c4f2b22c8c2
SeungWoo Yoo <dreamy1534@gmail.com>
2020-10-23 17:50:26 +0900

    feat(master): Implement record_screen

--- Autopilot/ImageGrab.py
+++ Autopilot/ImageGrab.py
@@ -1,15 +1,17 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
-
+import time
 
 def record_screen():
+    # Initialize timer to capture the interval between each process
+    last_time = time.time()
     # Record resolution : 800 x 600
     while True:
-        screen_pil = ImageGrab.grab(bbox=(0, 40, 800, 640))
-        screen_numpy = np.array(screen_pil.getdata(), dtype='uint8')\
-            .reshape((screen_pil.size[1], screen_pil.size[0], 3))
-        cv2.imshow('window', screen_numpy)
+        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
+        print('loop took {} seconds'.format(time.time() - last_time))
+        last_time = time.time()
+        cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
