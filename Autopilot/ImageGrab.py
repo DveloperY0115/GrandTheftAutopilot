24a93a714ea64724985d239e21a619b2b6def0d5
SeungWoo Yoo <dreamy1534@gmail.com>
2020-10-23 19:11:51 +0900

    feat(master): Improve capture performance and test

--- Autopilot/ImageGrab.py
+++ Autopilot/ImageGrab.py
@@ -1,18 +1,40 @@
 import numpy as np
-from PIL import ImageGrab
 import cv2
+import glob
+import mss
+import mss.tools
+from PIL import Image
 import time
 
-def record_screen():
-    # Initialize timer to capture the interval between each process
-    last_time = time.time()
+def record_screen(is_multi_monitor = False, target_monitor_idx = 1):
     # Record resolution : 800 x 600
-    while True:
-        screen = np.array(ImageGrab.grab(bbox=(0, 40, 800, 640)))
-        print('loop took {} seconds'.format(time.time() - last_time))
-        last_time = time.time()
-        cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    capturer = mss.mss()
+    monitor_number = target_monitor_idx
 
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+    mon = capturer.monitors[monitor_number]
+    # Using multiple monitors
+    if is_multi_monitor:
+        mon = capturer.monitors[monitor_number]
+
+    monitor = {
+        # Captures the top left-most 800 x 600 of the monitor region
+        "top": mon["top"] + 40,  # 100px from the top
+        "left": mon["left"],  # 100px from the left
+        "width": 800,
+        "height": 600,
+        "mon": monitor_number,
+    }
+
+    # Initialize timer to capture the interval between each process
+    previous_time = 0
+    while True:
+        frame = np.array(capturer.grab(monitor))
+        cv2.imshow('Captured Frame', frame)
+
+        if cv2.waitKey( 1 ) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
+
+        fps_txt = 'FPS: %.1f' % ( 1./(time.time() - previous_time))
+        previous_time = time.time()
+        print(fps_txt)
---
+++ Autopilot/main.py
@@ -0,0 +1,6 @@
+import ImageGrab
+import mss
+import mss.tools
+
+if __name__ == "__main__":
+    ImageGrab.record_screen(True    , 2)
