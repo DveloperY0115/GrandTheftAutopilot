f4f475b5b757128465aceb9f7f4505f5d2b05d26
SeungWoo Yoo <dreamy1534@gmail.com>
2020-10-25 16:11:00 +0900

    feat(master): Implement Object Detection using YOLO

---
+++ Autopilot/ObjectDetection.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+
+
+class YOLO_net():
+
+    # Constructor & Initialize network with parameters
+    def __init__(self):
+        self.Net = cv2.dnn.readNet("YOLOV3/yolov3-tiny.weights", "YOLOV3/yolov3-tiny.cfg")
+
+        self.classes = []
+        with open("YOLOV3/yolo.names", "r") as f:
+            self.classes = [line.strip() for line in f.readlines()]
+        self.layer_names = self.Net.getLayerNames()
+        self.output_layers = [self.layer_names[i[0] - 1] for i in self.Net.getUnconnectedOutLayers()]
+
+    def detect_objects(self, frame):
+
+        height, width, channel = frame.shape
+
+        # inputs to YOLO network
+        blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0),
+                                     True, crop=False)
+        self.Net.setInput(blob)
+        outputs = self.Net.forward(self.output_layers)
+
+        class_ids = []
+        confidences = []
+        boxes = []
+
+        for output in outputs:
+
+            for detection in output:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+
+                if confidence > 0.5:
+                    # Object is detected
+                    # The coordinate of boxes are normalized to be in between [0, 1]
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    dw = int(detection[2] * width)
+                    dh = int(detection[3] * height)
+
+                    # Rectangle coordinate
+                    x = int(center_x - dw / 2)
+                    y = int(center_y - dh / 2)
+                    boxes.append([x, y, dw, dh])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.45, 0.4)
+
+        for i in range(len(boxes)):
+            if i in indices:
+                x, y, w, h = boxes[i]
+                label = str(self.classes[class_ids[i]])
+                score = confidences[i]
+
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 5)
+                cv2.putText(frame, label, (x, y - 20), cv2.FONT_ITALIC, 0.5, (255, 255, 255), 1)
+
+        return frame
--- Autopilot/main.py
+++ Autopilot/main.py
@@ -1,6 +1,28 @@
-import ImageGrab
+import numpy as np
+import cv2
 import mss
 import mss.tools
+import time
+import pyautogui
+
+# User-defined functions and classes
+from DirectKeys import PressKey, W, A, S, D
+from ImageGrab import FrameCapture
+import ImageProcess
+from ObjectDetection import YOLO_net
+
 
 if __name__ == "__main__":
-    ImageGrab.record_screen(True    , 2)
+
+    network = YOLO_net()
+    sct = FrameCapture((800, 600), True, 1)
+
+    while True:
+        frame = sct.record_screen(ImageProcess.process_default)
+
+        frame = network.detect_objects(frame)
+        cv2.imshow('Does it work..?', frame)
+
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
