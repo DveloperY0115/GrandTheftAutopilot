f8ac497f3729fe0990872f69f2a5dc32ee8570b7
SeungWoo Yoo <dreamy1534@gmail.com>
2020-10-28 20:18:36 +0900

    refactor(master): Merging YOLOv5 into the project

--- Autopilot/main.py
+++ Autopilot/main.py
@@ -1,8 +1,9 @@
 import cv2
 import time
+import argparse
 
 # User-defined functions and classes
-from YOLOv4 import detectvideo
+from YOLOv5.yolov5.detect import detect
 from ImageGrab import FrameCapture
 import ImageProcess
 from ObjectDetection import YOLOv3_net
@@ -10,7 +11,27 @@ from ObjectDetection import YOLOv3_net
 
 if __name__ == "__main__":
 
-    network = YOLOv3_net()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument('--save-dir', type=str, default='inference/output', help='directory to save results')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    opt = parser.parse_args()
+    print(opt)
+
+    # Initialize model with parsed arguments
+
+    # network = YOLOv3_net()
     sct = FrameCapture((800, 600), True, 1)
 
     while True:
