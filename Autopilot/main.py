a98898b45cc380ba61426b1c6e58e59cb8f58ad3
Yoo SeungWoo <dreamy1534@gmail.com>
2020-11-15 00:55:06 +0900

    feat(master): Update main routine to use YOLOv5

--- Autopilot/main.py
+++ Autopilot/main.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import time
 import torch
@@ -8,22 +9,47 @@ from PIL import Image, ImageDraw
 from ImageGrab import FrameCapture
 import ImageProcess
 from ObjectDetection import YOLOv3_net
-from Ultra_Fast_Lane_Detection import LaneDetection
+from Autopilot.Ultra_Fast_Lane_Detection import LaneDetection
 
-
+# To avoid OpenMP related error
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 if __name__ == "__main__":
+
+    # Load YOLOv5 from Pytorch Hub
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True).fuse().eval()  # yolov5s.pt
+    model = model.autoshape()  # for autoshaping of PIL/cv2/np inputs and NMS
+
     network = YOLOv3_net()
     img_w, img_h = 800, 600
     sct = FrameCapture((img_w, img_h), True, 1)
 
     while True:
+        start_time = time.time()
+
         frame = sct.record_screen(ImageProcess.process_default)
-        out = network.detect_objects(frame)
-        frame = network.draw_boxes(frame, out[0], out[1], out[2])
-        frame = LaneDetection.detect_lane(frame)
-        print(type(frame))
+
+        with torch.no_grad():
+            prediction = model([frame], size=800)
+
+        for i, (img, pred) in enumerate(zip([frame], prediction)):
+            str = 'Image %g/%g: %gx%g ' % (i + 1, len([frame]), *img.shape[:2])
+            img = Image.fromarray(img.astype(np.uint8)) if isinstance(img, np.ndarray) else img  # from np
+            if pred is not None:
+                for c in pred[:, -1].unique():
+                    n = (pred[:, -1] == c).sum()  # detections per class
+                    str += '%g %ss, ' % (n, model.names[int(c)])  # add to string
+                for *box, conf, cls in pred:  # xyxy, confidence, class
+                    label = model.names[int(cls)] if hasattr(model, 'names') else 'class_%g' % cls
+                    # str += '%s %.2f, ' % (label, conf)  # label
+                    ImageDraw.Draw(img).rectangle(box, width=3)  # plot
+                frame = np.array(img)
+
+        # frame = LaneDetection.detect_lane(frame)
+        fps_txt = 'FPS: %.1f' % (1. / (time.time() - start_time))
+        print(fps_txt)
         cv2.imshow('Frame', frame)
+
 
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
