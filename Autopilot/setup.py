660244a81c2f79e8e9fdc8aaa32b39dfe10f7c2f
SeungWoo Yoo <dreamy1534@gmail.com>
2021-01-04 17:44:08 +0900

    feat(add-ci): Edit 'setup.py'

--- Autopilot/setup.py
+++ Autopilot/setup.py
@@ -4,5 +4,4 @@ setup(name='GrandTheftAutopilot',
       version='0.1',
       url='https://github.com/DveloperY0115/GrandTheftAutopilot',
       author='Seungwoo Yoo',
-      author_email='dreamy1534@gmail.com',
-      packages=['Autopilot'])
+      author_email='dreamy1534@gmail.com')
