8d1cb559c0b25a5dde8c885b9584481d87825b30
Scott Suk <scottsuk0306@gmail.com>
2021-01-07 00:48:53 +0900

    feat(datacollect): Reformat DataCollect.py. Fix minor errors.

--- Autopilot/DataCollect.py
+++ Autopilot/DataCollect.py
@@ -1,9 +1,6 @@
 import os
 import cv2
-import numpy as np
 import datetime
-# import Autopilot.Perception.ImageGrab as ImageGrab
-from PIL import ImageGrab, Image
 import win32api as wapi
 import pandas as pd
 import os
@@ -11,37 +8,35 @@ import os
 from core.img_process import Image_Processor
 
 # 800 * 600
-
-
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def capture_image():
-    drive_view = ImgProc.grab_screen()
-    mapview = drive_view[480:590, 5:160]
-    direction = drive_view[570:580, 15:25]
-    # self.drive_view = img
-    # Preprocess needed
-    return drive_view, mapview, direction
-
-
 class DataCollect:
-    # drive_view_bbox = (0, 40, 800, 640)
-    mapview_bbox = (5, 480, 160, 590)
-    direction_bbox = (15, 570, 25, 580)
-
     def __init__(self):
         self.keyList = ["\b"]
         for char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789,.'£$/\\":
             self.keyList.append(char)
         self.target_folder = './dataset/' + datetime.datetime.utcnow().strftime(
             "%y%m%d_%H-%M-%S") + "_" + "data/"
+        # Set all the folders
         try:
             if not os.path.exists(self.target_folder):
                 os.makedirs(self.target_folder)
-        except OSError:
-            print('Error: Creating directory. ' + self.target_folder)
+            if not os.path.exists(self.target_folder + "imgs"):
+                os.makedirs(self.target_folder + "imgs")
+            if not os.path.exists(self.target_folder + "imgs/drive_view"):
+                os.makedirs(self.target_folder + "imgs/drive_view")
+            if not os.path.exists(self.target_folder + "imgs/mapview"):
+                os.makedirs(self.target_folder + "imgs/mapview")
+            if not os.path.exists(self.target_folder + "imgs/direction"):
+                os.makedirs(self.target_folder + "imgs/direction")
 
+        except OSError:
+            print('Error: Creating directory.')
+
+    # Capture Key
+    # A, D: Represents steering angle in GTA
+    # S, E: Represents Start and End.
     def capture_key(self):
         for button in self.keyList:
             if wapi.GetAsyncKeyState(ord(button)):
@@ -49,16 +44,14 @@ class DataCollect:
                     return 'A'
                 elif button == 'D':
                     return 'D'
-                elif button == 'X':
-                    return 'X'
-                elif button == 'Q':
-                    return 'Q'
+                elif button == 'S':
+                    return 'S'
+                elif button == 'E':
+                    return 'E'
                 else:
                     return
         return
 
-    # save format: date_key (key is one of w,a,s,d)
-    # for example, 2020-11-17-20:50:34_w
     def save_data(self, drive_view_img, mapview_img, direction_img, control):
         # if control == 'w' or control == 'd': continue
         # save captured images in 'Autopilot/dataset/imgs/(file names)''
@@ -77,6 +70,9 @@ class DataCollect:
         if control is None:
             control = "None"
 
+        if control is "S" or "E":
+            control = "None"
+
         temp_dict = {'drive_view': drive_view_filename, 'mapview': mapview_filename,
                      'direction': direction_filename, 'control': control, 'speed': 0}
         return temp_dict
@@ -89,40 +85,6 @@ if __name__ == '__main__':
     dc = DataCollect()
     ImgProc = Image_Processor()
     while True:
-        key_input = dc.capture_key()
-        if key_input == 'X':
-            start_flag = True
-
-        if not start_flag:
-            continue
-
-        # Capture All images and Change them to RGB
-        # drive_view, mapview, direction = capture_image()
-        # drive_view = cv2.cvtColor(drive_view, cv2.COLOR_BGR2RGB)
-        # mapview = cv2.cvtColor(mapview, cv2.COLOR_BGR2RGB)
-        # direction = cv2.cvtColor(direction, cv2.COLOR_BGR2RGB)
-        drive_view = ImgProc.grab_screen()
-
-        cv2.imshow("Drive_view", drive_view)
-        # cv2.imshow("Mapview", mapview)
-        # cv2.imshow("Direction", direction)
-
-        # data_log = dc.save_data(drive_view, mapview, direction, key_input)
-        # print(data_log)
-        #
-        # index += 1
-        # data_dict[index] = data_log
-        #
-        # # When Q is pressed, all data are organized into a csv file.
-        # # Then, program is exited.
-        # if key_input == "Q":
-        #     cv2.destroyAllWindows()
-        #     dataset = pd.DataFrame.from_dict(data_dict, orient='index')
-        #     dataset_name = dc.target_folder + datetime.datetime.utcnow().strftime(
-        #         "%y%m%d_%H-%M-%S") + "_dataset.csv"
-        #     dataset.to_csv(dataset_name)
-        #     break
-
         if cv2.waitKey(25) & 0xFF == ord("q"):
             cv2.destroyAllWindows()
             dataset = pd.DataFrame.from_dict(data_dict, orient='index')
@@ -130,3 +92,30 @@ if __name__ == '__main__':
                 "%y%m%d_%H-%M-%S") + "_dataset.csv"
             dataset.to_csv(dataset_name)
             break
+
+        key_input = dc.capture_key()
+        # If press S, data starts to be collected
+        # If press E, data collecting stops
+        # After pressing E, Select cv2 screen and press Q.
+        # Program will save whole csv file and terminate.
+        if key_input == 'S':
+            start_flag = True
+        elif key_input == 'E':
+            start_flag = False
+
+        if not start_flag:
+            continue
+
+        drive_view = ImgProc.grab_screen()
+        mapview = drive_view[480:590, 5:160]
+        direction = drive_view[570:580, 15:25]
+
+        cv2.imshow("Drive_view", drive_view)
+        cv2.imshow("Mapview", mapview)
+        cv2.imshow("Direction", direction)
+
+        data_log = dc.save_data(drive_view, mapview, direction, key_input)
+        print(data_log)
+
+        index += 1
+        data_dict[index] = data_log
