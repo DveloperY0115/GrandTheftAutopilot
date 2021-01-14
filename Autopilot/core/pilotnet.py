a44dcaa23bec4fedef2cf393cb331feb3540e31c
SeungWoo Yoo <dreamy1534@gmail.com>
2021-01-14 22:06:49 +0900

    feat(pilotNet): Implement pilotNet using TF
    feat(pilotNet): Implement pilotNet using TF 2

--- Autopilot/core/pilotnet.py
+++ Autopilot/core/pilotnet.py
@@ -1,10 +1,50 @@
+import tensorflow as tf
+
+from tensorflow.keras import models
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, Dropout
+from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+
 class pilotNet:
 
-    def __init__(self):
+    def __init__(self, drop_out_rate):
         """
         Create and initialize pilotNet for vehicle control
         """
-        return
+        self.drop_out_rate = drop_out_rate
+        self.model = models.Sequential()
+        self.model.add(Rescaling(scale=1./255))
+
+        # three Conv2D layers with 5 x 5 kernels, and 2 x 2 strides
+        self.model.add(Conv2D(filters=24, kernel_size=(5, 5), strides=(2, 2),
+                              padding='valid', activation='relu', input_shape=(1, 200, 66, 3)))
+        self.model.add(Conv2D(filters=36, kernel_size=(5, 5), strides=(2, 2),
+                              padding='valid', activation='relu'))
+        self.model.add(Conv2D(filters=48, kernel_size=(5, 5), strides=(2, 2),
+                              padding='valid', activation='relu'))
+
+        # two Conv2D layers with 3 x 3 kernels, and no strides
+        self.model.add(Conv2D(filters=64, kernel_size=(3, 3),
+                              padding='valid', activation='relu'))
+        self.model.add(Conv2D(filters=64, kernel_size=(3, 3),
+                              padding='valid', activation='relu'))
+
+        # and data flows to three fully-connected layers
+        self.model.add(Flatten())   # (None, 1152)
+        self.model.add(Dense(units=1164))
+        self.model.add(Dropout(rate=self.drop_out_rate))
+        self.model.add(Dense(units=100))
+        self.model.add(Dropout(rate=self.drop_out_rate))
+        self.model.add(Dense(units=50))
+        self.model.add(Dropout(rate=self.drop_out_rate))
+        self.model.add(Dense(units=10))
+        self.model.add(Dropout(rate=self.drop_out_rate))
+        self.model.add(Dense(units=1))
+
+        # build the pilotNet model
+        self.model.build(input_shape=(1, 200, 66, 3))
+
+        self.model.summary()
+
 
     def __call__(self, *args, **kwargs):
         """
@@ -22,4 +62,5 @@ class pilotNet:
         :param kwargs:
         :return:
         """
-        return
+
+        return None
