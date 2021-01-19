93e75b6d7216b90cb4bc0b438dfacd09b6d2cdd6
SeungWoo Yoo <dreamy1534@gmail.com>
2021-01-19 20:07:40 +0900

    feat(img_process): Remove KNN from initialization

--- Autopilot/tests/test_img_process.py
+++ Autopilot/tests/test_img_process.py
@@ -3,15 +3,15 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 import cv2 as cv2
-from Autopilot.core import utils
-
-myCapture = utils.FrameCapture()
+from Autopilot.core import img_process
 
 while True:
-    frame = myCapture.record_screen()
-    frame = utils.FrameProcessor.process_default(frame)
-    cv2.imshow("test", frame)
+    processor = img_process.Image_Processor()
 
+    frame = processor.grab_screen()
+
+    cv2.imshow("searching for regions", frame)
+    
     if cv2.waitKey(25) & 0xFF == ord("q"):
         cv2.destroyAllWindows()
         break
